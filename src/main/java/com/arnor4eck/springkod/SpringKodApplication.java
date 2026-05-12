21c250f3b1690ef075ec5354de5dfe90b4875f28
arnor4eck <KivalinNik@mail.ru>
2026-05-12 12:57:30 +0300

    initial commit

---
+++ src/main/java/com/arnor4eck/springkod/SpringKodApplication.java
@@ -0,0 +1,13 @@
+package com.arnor4eck.springkod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKodApplication.class, args);
+    }
+
+}
