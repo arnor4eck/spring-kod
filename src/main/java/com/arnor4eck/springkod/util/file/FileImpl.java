ab673e4f67c62406b8be8a16e3dd0e895f035062
arnor4eck <KivalinNik@mail.ru>
2026-05-14 20:41:58 +0300

    create FileImpl is MultipartFile

---
+++ src/main/java/com/arnor4eck/springkod/util/file/FileImpl.java
@@ -0,0 +1,84 @@
+package com.arnor4eck.springkod.util.file;
+
+import com.arnor4eck.springkod.entity.datasitory_file.FileType;
+import org.jspecify.annotations.Nullable;
+import org.springframework.util.MimeType;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.Arrays;
+
+public final class FileImpl implements MultipartFile {
+
+    private final String name;
+    private final String originalName;
+    private final String contentType;
+    private final FileType fileType;
+    private final byte[] content;
+
+    public FileImpl(String name, String originalName,
+                    String contentType, FileType fileType, byte[] content) {
+        this.name = name;
+        this.originalName = originalName;
+        this.contentType = contentType;
+        this.content = Arrays.copyOf(content, content.length);
+        this.fileType = fileType;
+    }
+
+    public FileImpl(String name, String originalName,
+                    MimeType type, FileType fileType, byte[] content) {
+        this(name, originalName, type.getType(), fileType, content);
+    }
+
+    public FileImpl(MultipartFile file, FileType fileType) throws IOException {
+        this(file.getName(), file.getOriginalFilename(),
+            file.getContentType(), fileType,
+            file.getBytes());
+    }
+
+    public FileType getFileType() {
+        return fileType;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public @Nullable String getOriginalFilename() {
+        return originalName;
+    }
+
+    @Override
+    public @Nullable String getContentType() {
+        return contentType;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return content.length == 0;
+    }
+
+    @Override
+    public long getSize() {
+        return content.length;
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        return Arrays.copyOf(content, content.length);
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return new ByteArrayInputStream(content);
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException, IllegalStateException {
+        try(OutputStream out = new FileOutputStream(dest)) {
+            out.write(content);
+        }
+    }
+}
