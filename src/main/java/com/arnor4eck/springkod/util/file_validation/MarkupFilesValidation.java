222d669c352f3789eacbb7ec51f3ed260158b40f
arnor4eck <KivalinNik@mail.ru>
2026-05-17 23:23:41 +0300

    added FileValidation and implementations

---
+++ src/main/java/com/arnor4eck/springkod/util/file_validation/MarkupFilesValidation.java
@@ -0,0 +1,8 @@
+package com.arnor4eck.springkod.util.file_validation;
+
+public class MarkupFilesValidation implements FileValidation {
+    @Override
+    public boolean validate(byte[] bytes) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
